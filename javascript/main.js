865571ad6bfb85cd77a6c269de77f7ed5700dc8c
Manuel Hernández González <manuhergonz@gmail.com>
2024-01-10 21:31:54 +0100

    act

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,29 @@
-window.addEventListener('scroll', function()  {
-    let elements = document.getElementsByClassName('scroll-content');
-    let screenSize = window.innerHeight;
-    
-          for(var i = 0; i < elements.length; i++) {
-          var element = elements[i];
-  
-          if(element.getBoundingClientRect().top < screenSize) {
-            element.classList.add('visible');
-          } else {
-            element.classList.remove('visible');
-          }
-  
-        }
-  });
+// // CALCILADORA
+
+// const operacion = prompt('Bienvenido a la calculadora de Manu!!!, ingrese que operacion quiere hacer si es S es sumar si es R es restar si es M es multiplicar y si es D es divir');
+
+// const num1 = parseInt(prompt('ingrese el numero 1'));
+// const num2 = parseInt(prompt('ingrese el numero 2'));
+
+
+ 
+// if (operacion == 's'){
+//     resultado = num1 + num2
+//     prompt(`el resultado es : ${resultado}`);
+
+//    } else if(operacion == 'r'){
+//     resultado = num1 - num2
+//     prompt(`el Resultado es: ${resultado} `);
+
+//    }else if(operacion == 'm'){
+//     resultado = num1 * num2
+//      prompt(`el Resultado es: ${resultado} `);
+
+//    } else if(operacion == 'd'){
+//     resultado = num1 / num2
+//      prompt(`el Resultado es: ${resultado} `);
+
+//     }  else{
+//         prompt('La Operacion es Invalida!!!');
+   
+//  }
